c6136b54da38f12d2f5abb287f70a98a50d328f1
Leonardo Wolf <leonardowolf97@gmail.com>
2022-04-07 17:18:11 -0300

    Delete converter.js

--- converter.js
+++
@@ -1,8 +0,0 @@
-function convertToC() {
-
-    let F = document.getElementById("F").value;
-    const factor = 1.8;
-    let C = (F - 32) / factor;
-
-    document.getElementById("Result").innerHTML = F + " ºF = <strong>" + C + "ºF.</strong>";
-}
